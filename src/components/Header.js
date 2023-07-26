40e92f3242c56b80484993d128025530cc13ef5f
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-07-27 00:35:02 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,7 @@ const Header = () => {
 
   return (
     <header className="header">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6389784149267865"
-     crossorigin="anonymous"></script>
+    
       <div className="container">
         <div className="logo-wrapper">
           <div className="logo">
